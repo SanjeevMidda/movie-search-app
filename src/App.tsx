66f641b7f4793cd167f1a4d431734b9a80d11abc
Sanjeev Midda <s.midda1@gmail.com>
2026-05-25 17:12:01 +0100

    feat: create user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import "./styles/index.css";
 import { API } from "./config/api";
+import { useState } from "react";
 
 function App() {
   console.log(API);
 
+  const [searchQuery, setSearchQuery] = useState("");
+
+  console.log(searchQuery);
+
+  const userInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+    console.log(searchQuery);
+  };
+
   return (
     <div className="App">
       <h1>MOVIE SEARCH APP</h1>
+
+      <input
+        type="text"
+        id="userInput"
+        value={searchQuery}
+        onChange={userInput}
+      />
     </div>
   );
 }
