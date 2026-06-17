4de43b9f7c66a8e4d3d176d25c304282129ee766
Sanjeev Midda <s.midda1@gmail.com>
2026-06-17 23:21:32 +0100

    refactor: move all component tests into one file instead of separate

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,11 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import App from "../App";
-import useMovieSearch from "../hooks/useMovieSearch";
+import App from "./App";
+import useMovieSearch from "./hooks/useMovieSearch";
 
 jest.mock("../hooks/useMovieSearch");
 
+// loading state
 const mockedUseMovieSearch = useMovieSearch as jest.Mock;
 
 test("shows loading state", () => {
