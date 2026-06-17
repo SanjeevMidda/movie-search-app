500c68ce240c119e6bfe7984f428cd16ba576bad
Sanjeev Midda <s.midda1@gmail.com>
2026-06-17 18:50:01 +0100

    test: test loading state

---
+++ src/utilities/loadingStateRendering.test.tsx
@@ -0,0 +1,19 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import useMovieSearch from "../hooks/useMovieSearch";
+
+jest.mock("../hooks/useMovieSearch");
+
+const mockedUseMovieSearch = useMovieSearch as jest.Mock;
+
+test("shows loading state", () => {
+  mockedUseMovieSearch.mockReturnValue({
+    searchResults: [],
+    appStatus: "loading",
+  });
+
+  render(<App />);
+
+  expect(screen.getByText(/loading/i)).toBeInTheDocument();
+});
